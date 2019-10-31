9b36593e24d93eb0c5b67423b544dfc5b9d1c9ce
Scott Kwasnicki <kwasnickisw@mail.irsc.edu>
2019-10-31 18:34:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,15 +2,19 @@ class Song
   attr_accessor(:name, :artist, :genre)
   
     @@count = 0
+    @@genres = []
   
   def initialize
     @@count += 1
   end
   
   def self.count
+    @@count
   end
   
-  
+  def self.genres
+    @@genres
+  end
   
   
   
